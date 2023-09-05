21ea4aca722ce55b195dbf2cf0474988528adc80
suojae3 <suojae3@gmail.com>
2023-09-05 15:49:00 +0900

    feature/ add floating button

--- kickboard/kickboard/Controller/MapViewController.swift
+++ kickboard/kickboard/Controller/MapViewController.swift
@@ -1,42 +1,88 @@
-//
-//  MapViewController.swift
-//  kickboard
-//
-//  Created by ㅣ on 2023/09/05.
-//
-
 import UIKit
 import GoogleMaps
+import SnapKit
 
+
+//MARK: - Initilization
 class MapViewController: UIViewController {
 
-    @IBOutlet weak var mapView: GMSMapView!
-    
-    
+    private var floatingButton: UIButton!
+    private var mapView: GMSMapView!
+    private var camera: GMSCameraPosition!
+    private var locationManager: CLLocationManager!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupCameraPosition()
+        self.view = mapView
+        mapView.delegate = self
+        setupFloatingButton()
+    }
+}
+
+
+//MARK: - FloatingButton
+extension MapViewController {
+    
+    private func setupFloatingButton() {
+
+        floatingButton = UIButton(type: .custom)
+        floatingButton.setImage( UIImage(systemName: "scope"), for: .normal)
+        floatingButton.tintColor = .black
+        floatingButton.backgroundColor = .white
+        floatingButton.layer.shadowColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        floatingButton.layer.shadowOpacity = 20
+        floatingButton.layer.cornerRadius = 25
+        floatingButton.layer.borderColor = UIColor.black.cgColor
+        floatingButton.layer.borderWidth = 1.0
+        floatingButton.addTarget(self, action: #selector(floatingButtonTapped), for: .touchUpInside)
+        view.addSubview(floatingButton)
+        floatingButton.snp.makeConstraints { make in
+            make.width.height.equalTo(50)
+            make.bottom.equalTo(view).offset(-100)
+            make.trailing.equalTo(view).offset(-20)
+        }
+    }
+
+    @objc private func floatingButtonTapped() {
+        print("플로팅 버튼 눌림")
+    }
+}
+
+//MARK: - Camera Position
+extension MapViewController: GMSMapViewDelegate, CLLocationManagerDelegate {
+    
+    //위도 경도 변수 추후 할당
+    private func setupCameraPosition() {
         
-        let camera = GMSCameraPosition.camera(withLatitude: 40.785091, longitude: -73.968285, zoom: 15.0)
-        mapView.camera = camera
-
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 40.785091, longitude: -73.968285)
-        marker.title = "Rental"
-        marker.snippet = "Available : 5"
-        marker.map = mapView
-
+        
+        camera = GMSCameraPosition()
+        camera = GMSCameraPosition.camera(withLatitude: 37.58, longitude:126.98, zoom: 15)
+        mapView = GMSMapView(frame: .zero, camera: camera)
 
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Current Location 권한 획득
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+         if status == .authorizedWhenInUse {
+             locationManager.startUpdatingLocation()
+             mapView.isMyLocationEnabled = true
+             mapView.settings.myLocationButton = true
+         }
+     }
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
---
+++ kickboard/kickboard/Model/Data.swift
@@ -0,0 +1,39 @@
+//
+//import Foundation
+//
+//class StorageManager {
+//
+//    private let userDefaults = UserDefaults.standard
+//
+//    userDefaults.set(<#T##Any?#>, forKey: <#T##String#>)
+//
+//
+//
+//
+//}
+//
+//struct User: Codable {
+//    let userID: String
+//    let password: String
+//    let kickboardStatus: Bool
+//}
+//
+//struct Kickboard: Codable {
+//    let number: Int
+//    let kickboardStatus: Bool
+//    let locationX: Double
+//    let locationY: Double
+//    let userID: String
+//}
+//
+//struct UserRideRecord: Codable {
+//    let userID: String
+//    let kickboardNumber: Int
+//}
+//
+//
+////User
+////kickboard : 더미 데이터
+////UserRideRecord
+//
+//
