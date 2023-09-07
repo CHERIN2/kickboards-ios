c7c702a5ea1773e9a403fc584240329ec6fcf34c
jooYeon <conn0204@naver.com>
2023-09-07 11:35:55 +0900

    #7 Main - 공통 사용 actionSheet 만들기

---
+++ kickboard/kickboard/Controller/Utility.swift
@@ -0,0 +1,25 @@
+//
+//  Utility.swift
+//  kickboard
+//
+//  Created by Jooyeon Kang on 2023/09/07.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showActionSheet(title: String, completion: @escaping (Bool) -> Void) {
+        let cancel = UIAlertAction(title: "취소", style: .destructive)
+        let action = UIAlertAction(title: "확인", style: .default) { _ in
+            completion(true)
+        }
+        
+        let actionSheetController = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        
+        actionSheetController.addAction(cancel)
+        actionSheetController.addAction(action)
+        
+        present(actionSheetController, animated: true)
+    }
+}
