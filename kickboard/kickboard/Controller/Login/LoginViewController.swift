a11a98f76cba0b41042c6c43b29b8996797f67d3
Cherin <sechan963@gmail.com>
2023-09-07 17:18:31 +0900

    데이터 저장과 불러오기, 회원가입시 공란일 경우 알림창 띄우기 만등

--- kickboard/kickboard/Controller/Login/LoginViewController.swift
+++ kickboard/kickboard/Controller/Login/LoginViewController.swift
@@ -1,10 +1,37 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    // MARK: - initalization
+    @IBOutlet weak var loginTitle: UILabel!
+    @IBOutlet weak var loginDesciption: UILabel!
+    @IBOutlet weak var loginToSiginIn: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var typeIDField: UITextField!
+    @IBOutlet weak var typePWField: UITextField!
+    
+    // MARK: - userdefault set
+    
+    
+    // MARK: - UI SET
     override func viewDidLoad() {
-        
-        
         super.viewDidLoad()
+        
+        loginTitle.text = "로그인하기"
+        loginTitle.font = .boldSystemFont(ofSize: 20.0)
+        
+        loginDesciption.text = "아직 회원이 아니시라면,"
+        loginDesciption.textColor = .darkGray
+
+        loginToSiginIn.tintColor = .black
+        
+        loginButton.tintColor = .lightGray
+        
+        typeIDField.layer.borderColor = UIColor.black.cgColor
+        typeIDField.placeholder = "ID를 입력해주세요"
+        
+        typePWField.layer.borderColor = UIColor.black.cgColor
+        typePWField.placeholder = "비밀번호를 입력해주세요"
         
     }
 }
