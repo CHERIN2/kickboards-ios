7dfccfe76ee1e390ea0bf930dfcccfd3300ad636
jooYeon <conn0204@naver.com>
2023-09-08 16:14:26 +0900

    #46 사용 등록 페이지 - 시간 선택 창 공란 확인

--- kickboard/kickboard/Controller/RegistraionViewController.swift
+++ kickboard/kickboard/Controller/RegistraionViewController.swift
@@ -157,21 +157,25 @@ extension RegistraionViewController: UITableViewDataSource, UITableViewDelegate 
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        showActionSheet(title: "대여 하시겠습니까?") { [self] completion in
-            if completion {
-                let user = StorageManager.fetchUser()
-                guard let user = user else { return }
-                
-                self.selectedKickboard = self.kickboardsWithinRangeList[indexPath.row]
-                self.selectedKickboard?.kickboardStatus = true
-                self.selectedKickboard?.userID = user.userID
-                guard let kickboard = self.selectedKickboard else { return }
-                
-                let record = UserRideRecord(userID: user.userID, kickboardNumber: kickboard.number)
-                
-                StorageManager.updateUserKickboardStatus(isRiding: true)
-                StorageManager.updateKickboard(kickboard)
-                StorageManager.insertUserRideRecord(record)
+        if hoursTextField.text == "" {
+            showAlert(title: "필수항목", message: "시간을 입력해 주세요")
+        } else {
+            showActionSheet(title: "대여 하시겠습니까?") { [self] completion in
+                if completion {
+                    let user = StorageManager.fetchUser()
+                    guard let user = user else { return }
+                    
+                    self.selectedKickboard = self.kickboardsWithinRangeList[indexPath.row]
+                    self.selectedKickboard?.kickboardStatus = true
+                    self.selectedKickboard?.userID = user.userID
+                    guard let kickboard = self.selectedKickboard else { return }
+                    
+                    let record = UserRideRecord(userID: user.userID, kickboardNumber: kickboard.number)
+                    
+                    StorageManager.updateUserKickboardStatus(isRiding: true)
+                    StorageManager.updateKickboard(kickboard)
+                    StorageManager.insertUserRideRecord(record)
+                }
             }
         }
     }
--- kickboard/kickboard/Controller/Utility.swift
+++ kickboard/kickboard/Controller/Utility.swift
@@ -11,9 +11,23 @@ extension UIViewController {
     
     func showActionSheet(title: String, completion: @escaping (Bool) -> Void) {
         let cancel = UIAlertAction(title: "취소", style: .destructive)
-        let action = UIAlertAction(title: "확인", style: .default) 
+        let action = UIAlertAction(title: "확인", style: .default) { _ in
+            completion(true)
+        }
         
         let actionSheetController = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        
+        actionSheetController.addAction(cancel)
+        actionSheetController.addAction(action)
+        
+        present(actionSheetController, animated: true)
+    }
+
+    func showAlert(title: String, message: String?) {
+        let cancel = UIAlertAction(title: "취소", style: .destructive)
+        let action = UIAlertAction(title: "확인", style: .default)
+        
+        let actionSheetController = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
         actionSheetController.addAction(cancel)
         actionSheetController.addAction(action)
